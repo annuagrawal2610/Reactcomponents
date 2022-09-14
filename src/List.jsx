767a850e9b65646abc84eca5505ef0a53a6c83de
annuagrawal2610 <111873026+annuagrawal2610@users.noreply.github.com>
2022-09-14 17:18:43 +0530

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>dosa</li>
+      <li>idli</li>
+      <li>sambhar</li>
+    </ul>
+  );
+}
+
+export default List;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+
+//above function is component and its name starts with a capital letter always.
+ReactDom.render(<App />, document.getElementById("root"));
